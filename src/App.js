585edb5dc052cb606827c9f20e6406075a02687f
Robert Snyder <r.snyder49@gmail.com>
2019-01-18 00:18:34 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -38,4 +38,5 @@ const mapStateToProps = (state) => {
   return { items: state.items }
 }
 
-export default connect(mapStateToProps)(App);
+export default connect( state => ({ items: state.items }) )(App);
+
